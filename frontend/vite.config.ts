904d863da4d7a486789b4dcf1650a59da887afcd
sfrunza <frunza.sergiu3@gmail.com>
2025-02-27 19:39:17 -0500

    asd

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -15,7 +15,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       "/api/v1": {
-        target: process.env.VITE_API_URL || "http://localhost:3001",
+        target: process.env.VITE_BASE_URL,
         changeOrigin: true,
       },
     },
