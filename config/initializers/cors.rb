7c9305f3570d665a7495e4e8a72f122f3410e385
sfrunza <frunza.sergiu3@gmail.com>
2025-02-27 19:32:44 -0500

    asd

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -2,7 +2,8 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins "http://localhost:3000",
             "http://localhost:3001",
-            "http://localhost:4173"
+            "http://localhost:4173",
+            ENV["BASE_URL"]
 
     resource "*",
              headers: :any,
