0821a73742fcbbae9e6cba64015b37db00401bd2
sfrunza <frunza.sergiu3@gmail.com>
2025-02-27 19:21:54 -0500

    fix fonr prod

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -20,38 +20,38 @@ export default defineConfig({
       },
     },
   },
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          // Extract node_modules dependencies
-          if (id.includes("node_modules")) {
-            if (id.includes("react") || id.includes("react-dom")) {
-              return "vendor-react"; // React core libs
-            }
-            if (id.includes("react-router")) {
-              return "vendor-router"; // React Router
-            }
-            if (id.includes("redux") || id.includes("@reduxjs/toolkit")) {
-              return "vendor-redux"; // Redux
-            }
-            if (id.includes("@tanstack/react-query")) {
-              return "vendor-query"; // TanStack Query
-            }
-            if (id.includes("@shadcn") || id.includes("lucide-react")) {
-              return "vendor-ui"; // UI Libraries (ShadCN, Lucide)
-            }
-            return "vendor"; // Default vendor chunk
-          }
+  // build: {
+  //   rollupOptions: {
+  //     output: {
+  //       manualChunks(id) {
+  //         // Extract node_modules dependencies
+  //         if (id.includes("node_modules")) {
+  //           if (id.includes("react") || id.includes("react-dom")) {
+  //             return "vendor-react"; // React core libs
+  //           }
+  //           if (id.includes("react-router")) {
+  //             return "vendor-router"; // React Router
+  //           }
+  //           if (id.includes("redux") || id.includes("@reduxjs/toolkit")) {
+  //             return "vendor-redux"; // Redux
+  //           }
+  //           if (id.includes("@tanstack/react-query")) {
+  //             return "vendor-query"; // TanStack Query
+  //           }
+  //           if (id.includes("@shadcn") || id.includes("lucide-react")) {
+  //             return "vendor-ui"; // UI Libraries (ShadCN, Lucide)
+  //           }
+  //           return "vendor"; // Default vendor chunk
+  //         }
 
-          // Dynamic Import for Pages
-          if (id.includes("/src/pages/")) {
-            return `page-${id.split("/src/pages/")[1].split("/")[0]}`;
-          }
+  //         // Dynamic Import for Pages
+  //         if (id.includes("/src/pages/")) {
+  //           return `page-${id.split("/src/pages/")[1].split("/")[0]}`;
+  //         }
 
-          return null;
-        },
-      },
-    },
-  },
+  //         return null;
+  //       },
+  //     },
+  //   },
+  // },
 })
--- public/assets/page-account-CC84yMhA.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./vendor-react-CM67vIbR.js";function o(){return t.jsx("div",{children:"AccountPage"})}export{o as A};
