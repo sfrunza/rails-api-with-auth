e9b0c8b02c9ef35231c94fda1081778b213c2758
sfrunza <frunza.sergiu3@gmail.com>
2025-03-01 07:41:19 -0500

    sfsg

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,6 +88,12 @@ Rails.application.configure do
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
 
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.perform_caching = false
+  config.action_mailer.default_url_options = {
+    host: ENV["BASE_URL"],
+    protocol: "https"
+  }
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port: 587,
