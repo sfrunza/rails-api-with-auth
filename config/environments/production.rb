bc95e8c6f615753c77c69d3314ec7734da1d4523
sfrunza <frunza.sergiu3@gmail.com>
2025-03-01 07:48:18 -0500

    email prod config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -97,7 +97,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port: 587,
-    domain: ENV["BASE_URL"],
+    domain: "gmail.com",
     user_name: ENV["GMAIL_USERNAME"],
     password: ENV["GMAIL_PASSWORD"],
     authentication: "plain",
